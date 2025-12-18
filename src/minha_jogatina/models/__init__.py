922f180aaa567fab1dd48b532d186a47f28d64e3
Everton Pinheiro <everton.silva@olxbr.com>
2025-12-18 20:29:12 -0300

    feat: add initial implementation of Colecao class with game management methods

---
+++ src/minha_jogatina/models/__init__.py
@@ -0,0 +1,5 @@
+from .status import StatusJogo
+from .jogo import Jogo
+from .plataformas import JogoPC, JogoConsole, JogoMobile
+
+__all__ = ['StatusJogo', 'Jogo', 'JogoPC', 'JogoConsole', 'JogoMobile']
